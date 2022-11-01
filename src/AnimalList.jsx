8aacc7ed7830edec60b432983fa478f6d9ff6367
s-cumpalovic <stefancumpalovic98@gmail.com>
2022-11-01 19:36:36 +0100

    Animals list showing on launch of the application added a simple table HTML element in AnimalList component imported component in the App.js

---
+++ src/AnimalList.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const today = new Date();
+
+const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+
+
+
+const AnimalList = () => {
+    const animals = [
+        {name:'Jackie', species:'Dog', birth:date},
+        {name:'Pedja', species:'Monkey', birth:date},
+        {name:'Jack', species:'Aligator', birth:date},
+        {name:'Millie', species:'Lion', birth:date},
+        {name:'Jackie', species:'Bird', birth:date},
+    ];
+
+    return (
+        <div>
+            <h1>Animal list in the zoo:</h1>
+            <table>
+                <tr>
+                    <th>name: </th>
+                    <th>species: </th>
+                    <th>birth: </th>
+                </tr>
+            {animals.map((animal) => 
+                <tr>
+                        <th key="animal.id">{animal.name}</th>
+                        <th key="animal.id">{animal.species}</th>
+                        <th key="animal.id">{animal.birth}</th>
+
+                </tr>
+                    )}
+            </table>
+        </div>
+    )
+}
+
+export default AnimalList;
